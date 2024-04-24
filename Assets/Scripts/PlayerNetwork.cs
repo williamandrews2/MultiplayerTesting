ddd727521da414953c3ab485c445b131c5a0120c
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2024-04-23 19:18:18 -0500

    Created network variables and spawning objects.

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -7,8 +9,23 @@ public class PlayerNetwork : NetworkBehaviour
 {
     public float movementSpeed = 3;
 
+    [SerializeField] Transform spawnedObjectPrefab;
+    private Transform spawnedObjectTransform;
+
     Rigidbody rb;
 
+    private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    private NetworkVariable<FixedString64Bytes> message = new NetworkVariable<FixedString64Bytes>("Testing!");
+
+    // Use override OnNetworkSpawn for anything networking instead of Start or Awake.
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (int previousValue, int newValue) =>
+        {
+            Debug.Log(OwnerClientId + "; randomNumber: " + randomNumber.Value + "; Message: "+message.Value);
+        };
+    }
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -16,15 +33,43 @@ public class PlayerNetwork : NetworkBehaviour
  
     void Update()
     {
-        if(!IsOwner)
+        // Code only executes if they are the owner
+        if(!IsOwner) return;
+
+        // TESTING NETWORK VARIABLE
+        if (Input.GetKeyDown(KeyCode.T))
         {
-            return;
+            TestServerRpc();
         }
 
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            Destroy(spawnedObjectTransform.gameObject);
+        }
+
+        // Movement controls
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        // Update player position based on controls
         rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
 
+    }
+
+    // Another way to send data is through RPCs (Remote Procedure Call). Function name must contain "ServerRpc".
+    // This code runs only on the server, not the client!
+    [ServerRpc]
+    private void TestServerRpc()
+    {
+        Debug.Log("TestServerRpc " + OwnerClientId);
+        spawnedObjectTransform = Instantiate(spawnedObjectPrefab);
+        Debug.Log("Ball should be spawning");
+        spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true);
+    }
+
+    [ClientRpc]
+    private void TestClientRpc()
+    {
+        Debug.Log("TestClientRpc");
     }
 }
